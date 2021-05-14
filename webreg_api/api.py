6d4b1639d9992e7006f9e0c1b56423f05ff3781e
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-05-14 00:37:53 -0700

    Fixed bugs with API connecting to DB indefinetely

--- webreg_api/api.py
+++ webreg_api/api.py
@@ -7,36 +7,55 @@ from webreg_api import database, api_utils
 app = Flask(__name__)
 api = Api(app)
 
-# Database setup
-connection = database.setup_database_connection()
-cursor = connection.cursor()
-
 # Hello
+# Tells the user what endpoints exist
 class Hello(Resource):
     def get(self):
-        return {'message': 'Hello world'}
+        return {
+            'message': 'Make a GET request to any of these endpoints',
+            'Course endpoint': '/api/course/<int:code>/<int:year>/<string:quarter>',
+            'Department endpoint': '/api/dept/<string:dept>/<int:year>/<string:quarter>'
+        }
 
 # Course
 # Returns course info of the given course code
 class Course(Resource):
     def get(self, code, year, quarter):
-        table_name = database.build_table_name(year, quarter)
-        query = f'SELECT * FROM {table_name} WHERE code=%s'
-        cursor.execute(query, [code])
-        sql_result = cursor.fetchone()
-        response = api_utils.build_response(sql_result)
-        return response
+        response = None
+        connection = None
+        try:
+            connection = database.setup_database_connection()
+            cursor = connection.cursor()
+            table_name = database.build_table_name(year, quarter)
+            query = f'SELECT * FROM {table_name} WHERE code=%s'
+            cursor.execute(query, [code])
+            sql_result = cursor.fetchone()
+            response = api_utils.build_response(sql_result)
+        except:
+            response = api_utils.build_error_response(500)
+        finally:
+            connection.close()
+            return response
 
 # Department
 # Returns all courses within the given department
 class Department(Resource):
     def get(self, dept, year, quarter):
-        table_name = database.build_table_name(year, quarter)
-        query = f'SELECT * FROM {table_name} WHERE dept=%s'
-        cursor.execute(query, [dept])
-        sql_result = cursor.fetchall()
-        response = api_utils.build_response(sql_result)
-        return response
+        response = None
+        connection = None
+        try:
+            connection = database.setup_database_connection()
+            cursor = connection.cursor()
+            table_name = database.build_table_name(year, quarter)
+            query = f'SELECT * FROM {table_name} WHERE dept=%s'
+            cursor.execute(query, [dept])
+            sql_result = cursor.fetchall()
+            response = api_utils.build_response(sql_result)
+        except:
+            response = api.build_error_response(500)
+        finally:
+            connection.close()
+            return response
 
 # API Routes
 api.add_resource(Hello, '/')
@@ -45,4 +64,3 @@ api.add_resource(Department, '/api/dept/<string:dept>/<int:year>/<string:quarter
 
 if __name__ == '__main__':
     app.run(debug=True)
-    connection.close()

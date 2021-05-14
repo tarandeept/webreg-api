dcafa782dc8c5f0b0c7b887bfbdaac39cdaacff0
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-05-13 22:07:28 -0700

    Time functions now return time as strings rather than DATETIME objects

--- webreg_api/utils.py
+++ webreg_api/utils.py
@@ -6,7 +6,6 @@ def row_print(row):
         print(f'{k} | {v}')
 
 def extract_days(datetime) -> str:
-    ### REVISIT THIS FUNCTION AND CHANGE IT TO RETURN A LIST OF DIGITS [0,1,2,3,4,5,6]
     '''Given a string in the format: TuTh   11:00-12:20p, returns TuTh'''
     return datetime.split()[0].strip()
 
@@ -27,34 +26,25 @@ def append_am_pm(time) -> str:
 def extract_start_end_times(str_time) -> []:
     '''Returns a list of Time objects. Index 0 is the start time. Index 1 is the end time'''
     start = extract_start_time(str_time)
-    end = extract_end_time(str_time)
-    try:
-        diff = end - start
-        minutes = diff.total_seconds() / 60
-        if minutes > 300:
-            start = start + timedelta(hours = 12)
-        return [start.isoformat(), end.isoformat()]
-    except:
-        return [None, None]
+    end = 'TBA'
+    if start != 'TBA':
+        end = extract_end_time(str_time)
+    return [start, end]
 
 def extract_start_time(str_time):
     '''Returns a Time object representing the start time. If time is TBA, returns TBA'''
     try:
         time_range = str_time.split('-')
         time = time_range[0].split()[1].strip()
-        start = append_am_pm(time)
-        result = datetime.strptime(start, '%I:%M%p')
-        return result
+        return time
     except:
-        return str_time
+        return 'TBA'
 
 def extract_end_time(str_time):
     '''Returns a Time object representing the end time. If time is TBA, returns TBA'''
     try:
         time_range = str_time.split('-')
-        time = time_range[1].strip()
-        end = append_am_pm(time)
-        result = datetime.strptime(end, '%I:%M%p')
-        return result
+        time = time_range[-1].strip()
+        return time
     except:
-        return str_time
+        return 'TBA'

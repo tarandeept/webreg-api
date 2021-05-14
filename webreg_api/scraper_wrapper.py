8c87fc172f3c3f511d2c765bee41e5b1f224779b
Tarandeep Singh Thamrait <tthamrai@uci.edu>
2021-05-13 22:37:24 -0700

    GET request now encodes URL with params rather than raw string encoding

--- webreg_api/scraper_wrapper.py
+++ webreg_api/scraper_wrapper.py
@@ -29,38 +29,36 @@ import os
 from scraper import run_scraper
 import database
 
-BASE_URL = 'https://www.reg.uci.edu/perl/WebSoc/?YearTerm={YEAR-XX}&ShowComments=on&ShowFinals=on&Breadth=ANY&Dept={DEPT}&CourseNum=&Division=ANY&CourseCodes=&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Web+Results'
+BASE_URL = 'https://www.reg.uci.edu/perl/WebSoc/?ShowComments=on&ShowFinals=on&Breadth=ANY&CourseNum=&Division=ANY&CourseCodes=&InstrName=&CourseTitle=&ClassType=ALL&Units=&Days=&StartTime=&EndTime=&MaxCap=&FullCourses=ANY&FontSize=100&CancelledCourses=Exclude&Bldg=&Room=&Submit=Display+Web+Results'
 CURRENT_YEAR_TERM = '2021-92'
 CURRENT_YEAR = '2021'
 CURRENT_QTR = 'FALL'
 DEPTS = ['AC ENG', 'AFAM', 'ANATOMY', 'ANESTH', 'ANTHRO', 'ARABIC', 'ARMN', 'ART', 'ART HIS', 'ARTS',
         'ARTSHUM', 'ASIANAM', 'BANA', 'BATS', 'BIO SCI', 'BIOCHEM', 'BME', 'BSEMD', 'CAMPREC', 'CBE',
-        'CBEMS', 'CEM', 'CHC/LAT', 'CHEM', 'CHINESE', 'CLASSIC', 'CLT%26THY', 'COGS', 'COM LIT', 
+        'CBEMS', 'CEM', 'CHC/LAT', 'CHEM', 'CHINESE', 'CLASSIC', 'CLT&THY', 'COGS', 'COM LIT', 
         'COMPSCI', 'CRITISM', 'CRM/LAW', 'CSE', 'DANCE', 'DERM', 'DEV BIO', 'DRAMA', 'E ASIAN', 
         'EARTHSS', 'EAS', 'ECO EVO', 'ECON', 'ECPS', 'ED AFF', 'EDUC', 'EECS', 'EHS', 'ENGLISH', 
         'ENGR', 'ENGRCEE', 'ENGRMAE', 'ENGRMSE', 'EPIDEM', 'ER MED', 'EURO ST', 'FAM MED', 'FIN', 
-        'FLM%26MDA', 'FRENCH', 'GDIM', 'GEN%26SEX', 'GERMAN', 'GLBL ME', 'GLBLCLT', 'GREEK', 'HEBREW', 
-        'HINDI', 'HISTORY', 'HUMAN', 'HUMARTS', 'I%26C SCI', 'IN4MATX', 'INNO', 'INT MED', 'INTL ST', 
+        'FLM&MDA', 'FRENCH', 'GDIM', 'GEN&SEX', 'GERMAN', 'GLBL ME', 'GLBLCLT', 'GREEK', 'HEBREW', 
+        'HINDI', 'HISTORY', 'HUMAN', 'HUMARTS', 'I&C SCI', 'IN4MATX', 'INNO', 'INT MED', 'INTL ST', 
         'IRAN', 'ITALIAN', 'JAPANSE', 'KOREAN', 'LATIN', 'LAW', 'LINGUIS', 'LIT JRN', 'LPS', 'LSCI', 
-        'M%26MG', 'MATH', 'MED', 'MED ED', 'MED HUM', 'MGMT', 'MGMT EP', 'MGMT FE', 'MGMT HC', 
+        'M&MG', 'MATH', 'MED', 'MED ED', 'MED HUM', 'MGMT', 'MGMT EP', 'MGMT FE', 'MGMT HC', 
         'MGMTMBA', 'MGMTPHD', 'MIC BIO', 'MOL BIO', 'MPAC', 'MSE', 'MUSIC', 'NET SYS', 'NEURBIO', 
         'NEUROL', 'NUR SCI', 'OB/GYN', 'OPHTHAL', 'PATH', 'PED GEN', 'PEDS', 'PERSIAN', 'PHARM', 
-        'PHILOS', 'PHMD', 'PHRMSCI', 'PHY SCI', 'PHYSICS', 'PHYSIO', 'PLASTIC', 'PM%26R', 'POL SCI', 
-        'PORTUG', 'PP%26D', 'PSCI', 'PSY BEH', 'PSYCH', 'PUB POL', 'PUBHLTH', 'RADIO', 'REL STD', 
+        'PHILOS', 'PHMD', 'PHRMSCI', 'PHY SCI', 'PHYSICS', 'PHYSIO', 'PLASTIC', 'PM&R', 'POL SCI', 
+        'PORTUG', 'PP&D', 'PSCI', 'PSY BEH', 'PSYCH', 'PUB POL', 'PUBHLTH', 'RADIO', 'REL STD', 
         'ROTC', 'RUSSIAN', 'SOC SCI', 'SOCECOL', 'SOCIOL', 'SPANISH', 'SPPS', 'STATS', 'SURGERY', 
         'SWE', 'TAGALOG', 'TOX', 'UCDC', 'UNI AFF', 'UNI STU', 'UPPP', 'VIETMSE', 'VIS STD', 'WRITING']
 
 
-DEPTS = ['COMPSCI', 'SOC SCI', 'CHEM']  # Only 3 depts used for testing. Comment this when running for all depts
-
+# DEPTS = ['COMPSCI', 'SOC SCI', 'CHEM']  # Only 3 depts used for testing. Comment this when running for all depts
 
 
 
 def get_dept_courses(dept):
     '''Gets HTML course content for the given department'''
-    url = BASE_URL.replace('{YEAR-XX}', CURRENT_YEAR_TERM)
-    url = url.replace('{DEPT}', dept)
-    response = requests.get(url)
+    payload = {'YearTerm': CURRENT_YEAR_TERM, 'Dept': dept}
+    response = requests.get(BASE_URL, params=payload)
     html = response.text
     return html
 
